f63b01463040c1b1a4c6dad5b73f6d077fa79225
goterog <guillermo.a.otero@gmail.com>
2025-05-22 13:53:12 -0600

    mi primer commit

---
+++ BACKEND/src/api/map-location/controllers/map-location.ts
@@ -0,0 +1,7 @@
+/**
+ * map-location controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::map-location.map-location');
---
+++ BACKEND/src/api/map-location/services/map-location.ts
@@ -0,0 +1,7 @@
+/**
+ * map-location service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::map-location.map-location');
